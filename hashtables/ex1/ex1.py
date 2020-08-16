79463b0b003ee941bd2e0585a75e586a23eaf7bc
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-08-16 14:14:00 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,26 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
+    
+    for index in range(length):
+        x = limit - weights[index]
+
+        if x in dict:
+            subtract = dict[x]
+
+            new_lst = [index, subtract]
+            new_lst.sort(reverse=True)
+            return new_lst
+        else:
+            dict[weights[index]] = index
 
     return None
+
+
+
+# empty dict
+# go over the length of the of the weights
+# calculate by substracting the value at the current index
+# check the cache
+#  if the value is in the cache extract it and assign to a new variable
