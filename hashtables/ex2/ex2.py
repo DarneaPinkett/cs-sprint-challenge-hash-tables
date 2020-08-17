8461bca033c67d3bfdedabb61d4a8d16add3345e
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-08-16 21:27:34 -0400

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,32 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+dict = {}        
+
 
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    arr = []
 
-    return route
+    for ticket in tickets:
+        dict[ticket.source] = ticket.destination
+
+    arr.append(dict['NONE'])
+
+    for x in range(length):
+        if arr[x] in dict:
+            if dict[arr[x]] == arr[0]:
+                continue
+            arr.append(dict[arr[x]])
+        
+
+    return arr
+
+
+# create emptry array.
+# for each ticket in the ticket array, 
+# add the ticket source as the key, 
+# and the ticket destination as the value
